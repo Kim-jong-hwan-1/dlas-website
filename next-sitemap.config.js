58604aeb71a7fd106f5b7e22cef08dad7b71485f
kim-jong-hwan-1 <kim-jong-hwan-1@users.noreply.github.com>
2025-10-30 22:02:29 +0900

    chore: resolve merge conflicts

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://www.dlas.io',   // 도메인
-  generateRobotsTxt: true,          // robots.txt 자동 생성
-  changefreq: 'weekly',
-  priority: 0.7,
-};
